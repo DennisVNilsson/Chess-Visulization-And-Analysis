ec0503d1029751560b8d32d9186cbab212bad301
DennisVNilsson <dennis.vilhelm@outlook.com>
2021-06-12 16:01:21 +0200

    Heatmaps done, and parsing changed

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import chess
 import csv
 
 
-def write_csv(data):
-    with open('2021-03new.csv', 'a') as outfile:
+def write_csv(data, tofile):
+    with open(tofile, 'a') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(data)
 
@@ -14,7 +14,7 @@ def parseToGames(fromfile):
     pgn = open("C:\\Users\\Dennis\\PycharmProjects\\pythonProject1\\games-bank\\%s" % fromfile)
     colnames = ['PGN', 'Event', 'Result', 'BlackElo', 'WhiteElo',
                 'FEN', 'Termination', 'TimeControl', 'UTCTime']
-    write_csv(colnames)
+    write_csv(colnames, '2021-03new.csv')
     while True:
         game = chess.pgn.read_game(pgn)
         if game is None:
@@ -24,6 +24,9 @@ def parseToGames(fromfile):
         pgn_string = game.accept(exporter)
         write_csv([pgn_string, gameNode["Event"], gameNode["Result"], gameNode["BlackElo"], gameNode["WhiteElo"],
                    gameNode["FEN"], gameNode["Termination"], gameNode["TimeControl"], gameNode["UTCTime"]])
+
+def parse_and_write(data, fromfile):
+
 
 
 if __name__ == '__main__':
